b5c2759093f2b34a540767ccab4eed2dd7a8747a
jiajinlong <147525878@qq.com>
2015-05-14 21:57:31 +0800

    制作sidebar.php页面，设置style.css和functions.php文件。

--- archive.php
+++ archive.php
@@ -66,26 +66,5 @@
 		<!-- Blog Navigation -->
 		<p class="clearfix"> <a href="#" class="button float">&lt;&lt; Previous Posts</a> <a href="#" class="button float right">Newer Posts >></a> </p>
 	</div>
-	<!-- Column 2 / Sidebar -->
-	<div class="grid_4">
-		<h4>Catagories</h4>
-		<ul class="sidebar">
-			<li><a href="">So who are we?</a></li>
-			<li><a href="">Philosophy</a></li>
-			<li><a href="">History</a></li>
-			<li><a href="">Jobs</a></li>
-			<li><a href="">Staff</a></li>
-			<li><a href="">Clients</a></li>
-		</ul>
-		<h4>Archives</h4>
-		<ul class="sidebar">
-			<li><a href="">January 2010</a></li>
-			<li><a href="">December 2009</a></li>
-			<li><a href="">Novemeber 2009</a></li>
-			<li><a href="">October 2009</a></li>
-			<li><a href="">September 2009</a></li>
-			<li><a href="">August 2009</a></li>
-		</ul>
-	</div>
-	<div class="hr grid_12 clearfix">&nbsp;</div>
+	<?php get_sidebar(); ?>
 	<?php get_footer(); ?>
--- single.php
+++ single.php
@@ -72,33 +72,5 @@
 			</ul>
 		</form>
 	</div>
-	<!-- Column 2 / Sidebar -->
-	<div class="grid_4">
-		<h4>Catagories</h4>
-		<ul class="sidebar">
-			<li><a href="">So who are we?</a></li>
-			<li><a href="">Philosophy</a></li>
-			<li><a href="">History</a></li>
-			<li><a href="">Jobs</a></li>
-			<li><a href="">Staff</a></li>
-			<li><a href="">Clients</a></li>
-		</ul>
-		<h4>Latest News</h4>
-		<ul class="sidebar">
-			<li><a href="">Chickens rule the world</a></li>
-			<li><a href="">Chuck Bartowski is back!</a></li>
-			<li><a href="">Aurelius for sale</a></li>
-			<li><a href="">Themeforest goes bust</a></li>
-		</ul>
-		<h4>Archives</h4>
-		<ul class="sidebar">
-			<li><a href="">January 2010</a></li>
-			<li><a href="">December 2009</a></li>
-			<li><a href="">Novemeber 2009</a></li>
-			<li><a href="">October 2009</a></li>
-			<li><a href="">September 2009</a></li>
-			<li><a href="">August 2009</a></li>
-		</ul>
-	</div>
-	<div class="hr grid_12 clearfix">&nbsp;</div>
+	<?php get_sidebar(); ?>
 	<?php get_footer(); ?>
