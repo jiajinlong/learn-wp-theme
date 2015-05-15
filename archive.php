2c30a67e6ae4d58f496d61c98700c7eba23fe068
jiajinlong <147525878@qq.com>
2015-05-15 15:51:59 +0800

    主页直接复制到了存档页面，下一步看看需不需要修改。

--- archive.php
+++ archive.php
@@ -1,70 +1,30 @@
 <?php get_header(); ?>
-	<!-- Column 1 /Content  1列/内容-->
+	<!-- 1 列 / 内容 -->
 	<div class="grid_8">
-		<div class="sorting">
-			<div class="sort_by">
-				<h4>排序</h4>
-				<ul>
-					<li><a href="#">随机查看</a></li>
-					<li><a href="#">评论最多</a></li>
-					<li><a href="#">标题排序</a></li>
-				</ul>
-			</div>
-			<h4>浏览分类</h4>
-			<h2>WordPress</h2>
-			<p></p>
-			<p>使用 WordPress 可以搭建功能强大的网络信息发布平台，但更多的是应用于个性化的博客。针对博客的应用，WordPress 能让您省却对后台技术的担心，集中精力做好网站的内容。这里是露兜博客精心整理的 WordPress 相关内容，兴许会给您带来不小的帮助，希望您能喜欢。</p>
-		</div>
-		<!-- Blog Post 博客文章 -->
+		<div class="hr clearfix">&nbsp;</div>
+		<div class="hr clearfix">&nbsp;</div>
+		<!-- 博客文章 -->
+		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 		<div class="post">
-			<!-- Post Title 文章标题 -->
-			<h3 class="title"><a href="single.html">Loreum ipsium massa cras phasellus</a></h3>
-			<!-- Post Data 文章时间 -->
-			<p class="sub"><a href="#">News</a>, <a href="#">Products</a> &bull; 31st Sep, 09 &bull; <a href="#">1 Comment</a></p>
+			<!-- 文章标题 -->
+			<h3 class="title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h3>
+			<!-- 文章日期 -->
+			<p class="sub"><?php the_tags('标签: ', ', ', ''); ?> &bull; <?php the_time('Y-n-j'); ?> &bull; <?php comments_popup_link('0 条评论', '1 条评论', '% 条评论', '', '评论已关闭'); ?><?php edit_post_link('编辑', ' &bull; ', ''); ?></p>
 			<div class="hr dotted clearfix">&nbsp;</div>
-			<!-- Post Image 文章图片 -->
-			<img class="thumb" alt="" src="<?php bloginfo('template_url'); ?>/images/610x150.gif" />
-			<!-- Post Content 文章内容 -->
-			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. <b>Mauris vel porta erat.</b> Quisque sit amet risus at odio pellentesque sollicitudin. Proin suscipit molestie facilisis. Aenean vel massa magna. Proin nec lacinia augue. Mauris venenatis libero nec odio viverra consequat. In hac habitasse platea dictumst.</p>
-			<p>Cras vestibulum lorem et dui mollis sed posuere leo semper. Integer ac ultrices neque. Cras lacinia orci a augue tempor egestas. Sed cursus, sem ut vehicula vehicula, ipsum est mattis justo, at volutpat nibh arcu sit amet risus. Vestibulum tincidunt, eros ut commodo laoreet, arcu eros ultrices nibh, ac auctor est dui vel nibh.</p>
-			<!-- Read More Button 了解更多按钮-->
-			<p class="clearfix"><a href="single.html" class="button right"> Read More...</a></p>
+            <!-- 文章缩略图 -->
+			<!-- 文章内容 -->
+            <?php the_excerpt(); ?>
+			<!-- 了解更多按钮 -->
+			<p class="clearfix"><a href="<?php the_permalink(); ?>" class="button right">阅读全文</a></p>
 		</div>
 		<div class="hr clearfix">&nbsp;</div>
-		<!-- Blog Post 博客文章 -->
-		<div class="post">
-			<!-- Post Title -->
-			<h3 class="title"><a href="single.html">Loreum ipsium massa cras phasellus</a></h3>
-			<!-- Post Data -->
-			<p class="sub"><a href="#">News</a>, <a href="#">Products</a> &bull; 31st Sep, 09 &bull; <a href="#">1 Comment</a></p>
-			<div class="hr dotted clearfix">&nbsp;</div>
-			<!-- Post Image -->
-			<img class="thumb" alt="" src="<?php bloginfo('template_url'); ?>/images/610x150.gif" />
-			<!-- Post Content -->
-			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. <b>Mauris vel porta erat.</b> Quisque sit amet risus at odio pellentesque sollicitudin. Proin suscipit molestie facilisis. Aenean vel massa magna. Proin nec lacinia augue. Mauris venenatis libero nec odio viverra consequat. In hac habitasse platea dictumst.</p>
-			<p>Cras vestibulum lorem et dui mollis sed posuere leo semper. Integer ac ultrices neque. Cras lacinia orci a augue tempor egestas. Sed cursus, sem ut vehicula vehicula, ipsum est mattis justo, at volutpat nibh arcu sit amet risus. Vestibulum tincidunt, eros ut commodo laoreet, arcu eros ultrices nibh, ac auctor est dui vel nibh.</p>
-			<!-- Read More Button -->
-			<p class="clearfix"><a href="single.html" class="button right"> Read More...</a></p>
-		</div>
-		<div class="hr clearfix">&nbsp;</div>
-		<!-- Blog Post -->
-		<div class="post">
-			<!-- Post Title -->
-			<h3 class="title"><a href="single.html">Loreum ipsium massa cras phasellus</a></h3>
-			<!-- Post Data -->
-			<p class="sub"><a href="#">News</a>, <a href="#">Products</a> &bull; 31st Sep, 09 &bull; <a href="#">1 Comment</a></p>
-			<div class="hr dotted clearfix">&nbsp;</div>
-			<!-- Post Image -->
-			<img class="thumb" alt="" src="<?php bloginfo('template_url'); ?>/images/610x150.gif" />
-			<!-- Post Content -->
-			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. <b>Mauris vel porta erat.</b> Quisque sit amet risus at odio pellentesque sollicitudin. Proin suscipit molestie facilisis. Aenean vel massa magna. Proin nec lacinia augue. Mauris venenatis libero nec odio viverra consequat. In hac habitasse platea dictumst.</p>
-			<p>Cras vestibulum lorem et dui mollis sed posuere leo semper. Integer ac ultrices neque. Cras lacinia orci a augue tempor egestas. Sed cursus, sem ut vehicula vehicula, ipsum est mattis justo, at volutpat nibh arcu sit amet risus. Vestibulum tincidunt, eros ut commodo laoreet, arcu eros ultrices nibh, ac auctor est dui vel nibh.</p>
-			<!-- Read More Button -->
-			<p class="clearfix"><a href="single.html" class="button right"> Read More...</a></p>
-		</div>
-		<div class="hr clearfix">&nbsp;</div>
-		<!-- Blog Navigation -->
-		<p class="clearfix"> <a href="#" class="button float">&lt;&lt; Previous Posts</a> <a href="#" class="button float right">Newer Posts >></a> </p>
+		<?php endwhile; ?>
+		<!-- 博客导航 -->
+		<p class="clearfix"><?php previous_posts_link('&lt;&lt; 查看新文章', 0); ?> <span class="float right"><?php next_posts_link('查看旧文章 &gt;&gt;', 0); ?></span></p>
+		<?php else : ?>
+		<h3 class="title"><a href="#" rel="bookmark">未找到</a></h3>
+		<p>没有找到任何文章! </p>
+		<?php endif; ?>
 	</div>
 	<?php get_sidebar(); ?>
 	<?php get_footer(); ?>
