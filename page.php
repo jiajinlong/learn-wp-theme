208e18e579be8ee52108ce69084bd97fac5f2e8c
jiajinlong <147525878@qq.com>
2015-05-15 15:01:39 +0800

    添加标题，添加页面内容，添加评论框，添加查询语句。

--- page.php
+++ page.php
@@ -1,33 +1,16 @@
 <?php get_header(); ?>
 	<!-- Column 1 / Content -->
+	<?php if (have_posts()) : the_post(); update_post_caches($posts); ?>
+	<h2 class="grid_12 caption clearfix"><?php the_title(); ?></h2>
 	<div class="grid_8">
-		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vel porta erat. Quisque sit amet risus at odio pellentesque sollicitudin. Proin suscipit molestie facilisis. Aenean vel massa magna. Proin nec lacinia augue. Mauris venenatis libero nec odio viverra consequat. In hac habitasse platea dictumst.</p>
+		<?php the_content(); ?>
 		<!-- Contact Form -->
-		<form id="comment_form" action="" method="post">
-			<h3>Add a comment</h3>
-			<div class="hr dotted clearfix">&nbsp;</div>
-			<ul>
-				<li class="clearfix">
-					<label for="name">Your Name</label>
-					<input id="name" name="name" type="text" />
-				</li>
-				<li class="clearfix">
-					<label for="email">Your Email</label>
-					<input id="email" name="email" type="text" />
-				</li>
-				<li class="clearfix">
-					<label for="email">Your Website</label>
-					<input id="website" name="website" type="text" />
-				</li>
-				<li class="clearfix">
-					<label for="message">Comment</label>
-					<textarea id="message" name="message" rows="3" cols="40"></textarea>
-				</li>
-				<li class="clearfix">
-					<!-- Add Comment Button -->
-					<a type="submit" class="button medium black right">Add comment</a> </li>
-			</ul>
-		</form>
+		<?php comments_template(); ?>
 	</div>
+	<?php else : ?>
+	<div class="grid_8">
+	    没有找到你想要的页面！
+	</div>
+	<?php endif; ?>
 	<?php get_sidebar(); ?>
 	<?php get_footer(); ?>
