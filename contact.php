1833ad8321b95530d4ca6ae9421eaefe28325632
jiajinlong <147525878@qq.com>
2015-05-15 15:05:20 +0800

    添加联系页面代码。

--- contact.php
+++ contact.php
@@ -1,66 +1,55 @@
+<?php
+/*
+Template Name: contract
+*/
+?>
 <?php get_header(); ?>
+	<!-- Caption Line -->
+	<?php if (have_posts()) : the_post(); update_post_caches($posts); ?>
+	<h2 class="grid_12 caption clearfix"><?php the_title(); ?></h2>
+	<div class="hr grid_12 clearfix">&nbsp;</div>
 	<!-- Column 1 / Content -->
 	<div class="grid_8">
-		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vel porta erat. Quisque sit amet risus at odio pellentesque sollicitudin. Proin suscipit molestie facilisis. Aenean vel massa magna. Proin nec lacinia augue. Mauris venenatis libero nec odio viverra consequat. In hac habitasse platea dictumst.</p>
-		<!-- Contact Form -->
-		<form action='index.html' method='post' id='contact_form'>
-			<h3>Contact Form</h3>
-			<div class="hr dotted clearfix">&nbsp;</div>
-			<ul>
-				<li class="clearfix">
-					<label for="name">Name</label>
-					<input type='text' name='name' id='name' />
-					<div class="clear"></div>
-					<p id='name_error' class='error'>Insert a Name</p>
-				</li>
-				<li class="clearfix">
-					<label for="email">Email Address</label>
-					<input type='text' name='email' id='email' />
-					<div class="clear"></div>
-					<p id='email_error' class='error'>Enter a valid email address</p>
-				</li>
-				<li class="clearfix">
-					<label for="subject">Subject</label>
-					<input type='text' name='subject' id='subject' />
-					<div class="clear"></div>
-					<p id='subject_error' class='error'>Enter a message subject</p>
-				</li>
-				<li class="clearfix">
-					<label for="message">Message</label>
-					<textarea name='message' id='message' rows="30" cols="30"></textarea>
-					<div class="clear"></div>
-					<p id='message_error' class='error'>Enter a message</p>
-				</li>
-				<li class="clearfix">
-					<p id='mail_success' class='success'>Thank you. I'll get back to you as soon as possible.</p>
-					<p id='mail_fail' class='error'>Sorry, an error has occured. Please try again later.</p>
-					<div id="button">
-						<input type='submit' id='send_message' class="button" value='Submit' />
-					</div>
-				</li>
-			</ul>
-		</form>
+		<?php the_content(); ?>
+		<?php comments_template(); ?>
 	</div>
+	<?php else : ?>
+	<div class="grid_8">
+		Ã»ÓÐÈÎºÎÎÄÕÂ£¡
+	</div>
+	<?php endif; ?>
 	<!-- Column 2 / Sidebar -->
-	<div class="grid_4">
-		<h4>Catagories</h4>
-		<ul class="sidebar">
-			<li><a href="">So who are we?</a></li>
-			<li><a href="">Philosophy</a></li>
-			<li><a href="">History</a></li>
-			<li><a href="">Jobs</a></li>
-			<li><a href="">Staff</a></li>
-			<li><a href="">Clients</a></li>
+	<div class="grid_4 contact">
+		<!-- Adress and Phone Details -->
+		<h4>Address and Phone</h4>
+		<div class="hr dotted clearfix">&nbsp;</div>
+		<ul>
+			<li> <strong>Your Company Name</strong><br />
+				1458 Sample Road, Redvalley<br />
+				City (State) H4Q 1J7<br />
+				Country<br />
+				<br />
+			</li>
+			<li>USA - (888) 888-8888</li>
+			<li>Worldwide - (888) 888-8888</li>
 		</ul>
-		<h4>Archives</h4>
-		<ul class="sidebar">
-			<li><a href="">January 2010</a></li>
-			<li><a href="">December 2009</a></li>
-			<li><a href="">Novemeber 2009</a></li>
-			<li><a href="">October 2009</a></li>
-			<li><a href="">September 2009</a></li>
-			<li><a href="">August 2009</a></li>
+		<!-- Email Addresses -->
+		<h4>Emails</h4>
+		<div class="hr dotted clearfix">&nbsp;</div>
+		<ul>
+			<li>General - <a href="mailto:info@yourcompany.com">info@yourcompany.com</a></li>
+			<li>Sales - <a href="mailto:sales@yourcompany.com">sales@yourcompany.com</a></li>
+		</ul>
+		<!-- Social Profile Links -->
+		<h4>Social Profiles</h4>
+		<div class="hr dotted clearfix">&nbsp;</div>
+		<ul>
+			<li class="float"><a href="#"><img alt="" src="images/twitter.png" title="Twitter" /></a></li>
+			<li class="float"><a href="#"><img alt="" src="images/facebook.png" title="Facebook" /></a></li>
+			<li class="float"><a href="#"><img alt="" src="images/stumbleupon.png" title="StumbleUpon" /></a></li>
+			<li class="float"><a href="#"><img alt="" src="images/flickr.png" title="Flickr" /></a></li>
+			<li class="float"><a href="#"><img alt="" src="images/delicious.png" title="Delicious" /></a></li>
 		</ul>
 	</div>
 	<div class="hr grid_12 clearfix">&nbsp;</div>
-	<?php get_footer(); ?>
+<?php get_footer(); ?>
